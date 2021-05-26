640d581ec33baed8f1f24fe74128cc1cea6a03b9
shuklajivivek <vivekshukla.nit@gmail.com>
2021-05-26 16:05:45 +0530

    basic survey layout testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import Header from './MyComponents/Header';
 function App() {
+  let data = [
+		{
+    "id": "1",
+    "question": "what is your name?",
+    "answer_type": "text",
+  },
+  {
+    "id": "2",
+    "question": "what is your age?",
+    "answer_type": "text",
+  },
+  {
+    "id": "3",
+    "question": "have you graduated?",
+    "answer_type": "checkbox",
+  },
+  {
+    "id": "4",
+    "question": "what is your graduation school name?",
+    "answer_type": "text",
+  }
+];
   return (
     <div>
-      <Header/>
+      {
+      data.map((item)=>{
+      return (<Header key = {item.id} question = {item.question} answer_type = {item.answer_type} question_id = {item.id}/> )  
+      })
+      }
     </div>
   );
 }
--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -1,7 +1,25 @@
 import React from 'react';
-function Header() {
+import { Container, InputGroup } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
+import {FormControl} from 'react-bootstrap'; 
+function Header(props) {
+  let input;
+  if(props.answer_type === 'text'){
+    input = <InputGroup><FormControl as="textarea" aria-label="With textarea" /></InputGroup>
+  }
+  else{
+    input = <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+  }  
   return (
-    <p>Header component</p>
+    <Container>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Question {props.question_id}</Form.Label>
+          <Form.Control value = {props.question}  />
+        </Form.Group>
+      </Form>
+      {input}
+    </Container>
   );
 }
 export default Header;
